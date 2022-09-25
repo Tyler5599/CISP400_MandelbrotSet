20344d85bcae7014393767c35ce9cd4c78e72dff
JUSTIN M ORTIZ <justino_95242@yahoo.com>
2022-09-24 23:09:11 -0700

    Add files via upload

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -1,5 +1,6 @@
 #include "ComplexPlane.h"
 #include <SFML/Graphics.hpp>
+#include <sstream>
 
 using namespace std;
 using namespace sf;
@@ -37,4 +38,24 @@ void ComplexPlane::setCenter(Vector2f coord)
 void ComplexPlane::setMouseLocation(Vector2f coord)
 {
 	m_mouseLocation = coord;
+}
+
+View cgetView()
+{
+	//code
+}
+void ComplexPlane::loadText(Text&)
+{
+	Text;
+	stringstream mystream;
+	mystream << "Mandelbrot Set" << endl << "Center: (0,0)" << endl << "Cursor: (" << m_mouseLocation.x << "," << m_mouseLocation.y << ")" << endl 
+	<< "Left-click to Zoom in" << endl << "Right-click to Zoom out" << endl;
+}
+size_t ComplexPlane::countIterations(Vector2f)
+{
+	//code
+}
+void ComplexPlane::iterationsToRGB(size_t count, Uint8 r, Uint8 g, Uint8 b)
+{
+	//code
 }
--- code/main.cpp
+++ code/main.cpp
@@ -7,5 +7,82 @@ using namespace sf;
 
 int main()
 {
+	//variables
+	float aspect, width, height;
+	//calculating aspect ratio by dividing or values that were rturned from get width and height functions
+	width =	VideoMode::getDesktopMode().width;
+	height = VideoMode::getDesktopMode().height;
+	aspect = height / width;
+
+	VideoMode vm(1920, 1080);
+	RenderWindow window(vm, "Mandelbrot!!!", Style::Default);
+	ComplexPlane plane1(aspect);
+	sf::Font font;
+	if (!font.loadFromFile("arial.ttf"))
+	{
+		// error...
+	}
+	sf::Text text1;
+	text1.setFont(font);
+	text1.setCharacterSize(30);
+	text1.setStyle(sf::Text::Regular);
+	VertexArray ourArray(Points,width*height);
+	enum class States
+	{
+		CALCULATING, DISPLAYING
+	};
+	States::CALCULATING;
+	while (window.isOpen())
+	{
+		//Process events
+			sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// Close window: exit
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		//close is esc key is pressed
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+		//close is esc key is pressed
+		if (Mouse::isButtonPressed(Mouse::Left))
+		{
+			Vector2f points(Mouse::getPosition().x, Mouse::getPosition().y);
+			plane1.setCenter(points);
+			plane1.zoomIn();
+			States::CALCULATING;
+		}
+		if (Mouse::isButtonPressed(Mouse::Right))
+		{
+			Vector2f points(Mouse::getPosition().x, Mouse::getPosition().y);
+			plane1.setCenter(points);
+			plane1.zoomOut();
+			States::CALCULATING;
+		}
+		/*Handle Event::MouseMovedUse mapPixelToCoords to find the Vector2f coordinate in the ComplexPlane View that corresponds to the screen mouse location
+		setMouseLocation on the ComplexPlane object to store this coordinate This will be used later to display the mouse coordinates as it moves*/
+		if(event.MouseMoved)
+		{
+			Vector2f points(Mouse::getPosition().x, Mouse::getPosition().y);
+			plane1.setMouseLocation(points);
+		}
+		if (States::CALCULATING == States::CALCULATING)
+		{
+			for(int j = 0; j < height; j++)
+			{
+				for (int i = 0; i < width; i++)
+				{
+					ourArray[j + i * width].position = { (float)j,(float)i };
+				}
+			}
+		}
+		// Clear screen
+		window.clear();
+		// Update the window
+		window.display();
+	}
 	return 0;
 };
